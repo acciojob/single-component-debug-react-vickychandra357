49aba0f8afa2e86206c7c44a83285538ea1c782b
tonystark <chandravicky288@gmail.com>
2023-05-18 16:15:15 +0530

    sol

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ const App=()=>
 {
     return(
         <div>
-            <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+            <p id="main">Now I can render any React component on any DOM node I want using ReactDOM.render</p>
         </div>
     )
 }
